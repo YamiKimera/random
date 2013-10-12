5f2cb6de97e51291e9f6a65a3d33f92fa3cee274
YamiKimera <jesterrabbit969@gmail.com>
2013-10-12 10:21:06 +0930

    random

---
+++ js/eating.js
@@ -0,0 +1,16 @@
+$("#eat").click(function(){
+    $("#RESULTS").prepend("OM NOM NOM NOM <br/>");
+});
+$("#sleep").click(function(){
+    $("#RESULTS").prepend("ZZZ... zzz ZZZ <br/>");
+});
+$("#jump").click(function(){
+    $("#RESULTS").prepend("'you landed on the moon and suffocated' <br/>");
+});
+$("#walk").click(function(){
+    $("#RESULTS").prepend("step step step <br/>");
+});
+});
+$("#combust").click(function(){
+    $("#RESULTS").prepend("'you blew up' <br/>");
+});
